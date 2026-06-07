125c982c4a12b7ea3225d377f6281f4238ad3452
Momchil Velikov <momchil.velikov@gmail.com>
2026-06-07 14:26:20 +0100

    Move bit-engines outside the benchmarking functions
    Compilers may optimise the entire bit-engine state to be held
    in registers for the duration of the benchmark, which is hardly
    representative of real-world use.

--- benchmarks/random-benchmark.cc
+++ benchmarks/random-benchmark.cc
@@ -4,43 +4,46 @@
 
 namespace {
 
+std::mt19937_64 g_mt_eng(12345);
+lightrand::xoshiro256starstar g_xo_eng(12345);
+
 // --- PRNG Engines ---
 
 void BM_Engine_MT19937_64(benchmark::State &state) {
-  std::mt19937_64 eng(12345);
+  g_mt_eng.seed(12345);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(eng());
+    benchmark::DoNotOptimize(g_mt_eng());
 }
 BENCHMARK(BM_Engine_MT19937_64);
 
 void BM_Engine_Xoshiro256StarStar(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
+  g_xo_eng.seed(12345);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(eng());
+    benchmark::DoNotOptimize(g_xo_eng());
 }
 BENCHMARK(BM_Engine_Xoshiro256StarStar);
 
 // --- Uniform Integer Distribution ---
 
 void BM_StdUniformInt(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
+  g_xo_eng.seed(12345);
   std::uniform_int_distribution<int> dist(10, 1000);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_xo_eng));
 }
 BENCHMARK(BM_StdUniformInt);
 
 void BM_StdUniformInt_MT(benchmark::State &state) {
-  std::mt19937_64 eng(12345);
+  g_mt_eng.seed(12345);
   std::uniform_int_distribution<int> dist(10, 1000);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_mt_eng));
 }
 BENCHMARK(BM_StdUniformInt_MT);
 
 void BM_LightrandUniformInt(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
-  lightrand::generator gen(eng);
+  g_xo_eng.seed(12345);
+  lightrand::generator gen(g_xo_eng);
   for (auto _ [[maybe_unused]] : state)
     benchmark::DoNotOptimize(gen.uniform<int>(10, 1000));
 }
@@ -49,24 +52,24 @@ BENCHMARK(BM_LightrandUniformInt);
 // --- Uniform Real Distribution ---
 
 void BM_StdUniformReal(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
+  g_xo_eng.seed(12345);
   std::uniform_real_distribution<double> dist(0.0, 1.0);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_xo_eng));
 }
 BENCHMARK(BM_StdUniformReal);
 
 void BM_StdUniformReal_MT(benchmark::State &state) {
-  std::mt19937_64 eng(12345);
+  g_mt_eng.seed(12345);
   std::uniform_real_distribution<double> dist(0.0, 1.0);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_mt_eng));
 }
 BENCHMARK(BM_StdUniformReal_MT);
 
 void BM_LightrandUniformReal(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
-  lightrand::generator gen(eng);
+  g_xo_eng.seed(12345);
+  lightrand::generator gen(g_xo_eng);
   for (auto _ [[maybe_unused]] : state)
     benchmark::DoNotOptimize(gen.uniform<double>());
 }
@@ -75,24 +78,24 @@ BENCHMARK(BM_LightrandUniformReal);
 // --- Normal Distribution ---
 
 void BM_StdNormal(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
+  g_xo_eng.seed(12345);
   std::normal_distribution<double> dist(0.0, 1.0);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_xo_eng));
 }
 BENCHMARK(BM_StdNormal);
 
 void BM_StdNormal_MT(benchmark::State &state) {
-  std::mt19937_64 eng(12345);
+  g_mt_eng.seed(12345);
   std::normal_distribution<double> dist(0.0, 1.0);
   for (auto _ [[maybe_unused]] : state)
-    benchmark::DoNotOptimize(dist(eng));
+    benchmark::DoNotOptimize(dist(g_mt_eng));
 }
 BENCHMARK(BM_StdNormal_MT);
 
 void BM_LightrandNormal(benchmark::State &state) {
-  lightrand::xoshiro256starstar eng(12345);
-  lightrand::generator gen(eng);
+  g_xo_eng.seed(12345);
+  lightrand::generator gen(g_xo_eng);
   for (auto _ [[maybe_unused]] : state)
     benchmark::DoNotOptimize(gen.normal<double>());
 }
